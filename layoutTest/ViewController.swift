b7a8ee2ab4b2f9e60b2b37e129cfbb577c499d98
Nobuo Danjou <danjou@soffritto.org>
2021-05-28 09:14:59 -0400

    Initial Commit

---
+++ layoutTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  layoutTest
+//
+//  Created by 檀上伸郎 on 2021/05/28.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
